3b1a0acd55ab18d371ec436ad922eeed2e7e547a
Ganim-Bicoli <60735102+Ganim-Bicoli@users.noreply.github.com>
2020-05-13 17:54:29 +0200

    Add project files.

---
+++ KillList/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KillList.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            CharecterServiceRefrence.CharecterServiceClient klient = new CharecterServiceRefrence.CharecterServiceClient();
+            List<CharecterServiceRefrence.CharecterData> ListOfChar = klient.GetData().ToList();
+
+
+
+            return View(ListOfChar);
+        }
+    }
+}
---
+++ KillList/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KillList.MvcApplication" Language="C#" %>
